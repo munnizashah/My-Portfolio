bf7d6ac5f4ca6230f061dd47a506bd791cf55e07
Munniza <you@example.com>
2023-05-18 21:48:15 +0200

    navbar setup

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
   return (
     <>
-      <h1>hello testing again</h1>
+      <Navbar />
     </>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,173 @@
+"use client";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState("#212122");
+  const [linkColor, setLinkColor] = useState("#1f2937");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      router.asPath === "/portfolio" ||
+      router.asPath === "/spotify" ||
+      router.asPath === "/todoapp" ||
+      router.asPath === "/modernpage"
+    ) {
+      setNavBg("transparent");
+      setLinkColor("#ecf0f3");
+    } else {
+      setNavBg("#212122");
+      setLinkColor("#ecf0f3");
+    }
+  }, [router]);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+  }, []);
+
+  return (
+    <div
+      style={{ backgroundColor: `${navBg}` }}
+      className={
+        shadow
+          ? "fixed w-full h-20 shadow-xl z-[100]"
+          : "fixed w-full h-20 z-[100]"
+      }
+    >
+      <div className="flex justify-end pr-8 items-center w-full h-full px-2 2xl:px-16">
+        <div>
+          <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
+            <Link href="/#main" scroll={false}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/#about" scroll={false}>
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/#skills" scroll={false}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/#projects" scroll={false}>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/#contact" scroll={false}>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="md:hidden" onClick={handleNav}>
+          <AiOutlineMenu size={25} />
+        </div>
+      </div>
+
+      <span
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#1B1B1B] p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 h-screen ease-in duration-500"
+          }
+        >
+          <div
+            onClick={handleNav}
+            className="flex w-full items-center justify-end"
+          >
+            <div className=" rounded-full shadow-lg shadow-black p-3 cursor-pointer">
+              <AiOutlineClose style={{ color: "white" }} />
+            </div>
+          </div>
+          <div className="border-b border-gray-600 my-4">
+            <p className="w-[85%] md:w-[90%] py-4">
+              Lets build a great portfolio
+            </p>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/#main" scroll={false}>
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Home
+                </li>
+              </Link>
+              <Link href="/#about" scroll={false}>
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  About
+                </li>
+              </Link>
+              <Link href="/#skills" scroll={false}>
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Skills
+                </li>
+              </Link>
+              <Link href="/#projects" scroll={false}>
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Projects
+                </li>
+              </Link>
+              <Link href="/#contact" scroll={false}>
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Lets connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-black p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <Link
+                    target="_blank"
+                    href="https://www.linkedin.com/in/munniza-shah-4324b0233/"
+                  >
+                    <FaLinkedinIn style={{ fill: "white" }} />
+                  </Link>
+                </div>
+                <div className="rounded-full shadow-lg shadow-black p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <Link target="_blank" href="https://github.com/munnizashah">
+                    <FaGithub style={{ fill: "white" }} />
+                  </Link>
+                </div>
+                <div className="rounded-full shadow-lg shadow-black p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <Link target="_blank" href="mailto: shahmunniza@gmail.com">
+                    <AiOutlineMail style={{ fill: "white" }} />
+                  </Link>
+                </div>
+                {/* <div className="rounded-full shadow-lg shadow-black p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsFillPersonLinesFill style={{ fill: "white" }} />
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </span>
+    </div>
+  );
+};
+
+export default Navbar;
