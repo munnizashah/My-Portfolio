c07b32e9c4ee1065f1a4c05ba43444efaf4e09f5
Munniza <you@example.com>
2023-08-20 01:42:56 +0200

    testing scroll problem

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    scrollRestoration: false,
+  },
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
